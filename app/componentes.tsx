f9939fb416fa5a9a7120017fc1ce852c1d44aa70
gabrielpasso2008 <gabrielpasssos@gmail.com>
2026-02-10 20:06:36 -0300

    primeira aula do react

---
+++ app/componentes.tsx
@@ -0,0 +1,15 @@
+export function Ohhh(props){  
+    return(
+        <div>
+          <header > 
+            <ul >
+                <li><a>{props.itens[0]}</a></li>
+                <li><a>{props.itens[1]}</a></li>
+                <li><a>{props.itens[2]}</a></li>
+                <li><a>{props.itens[3]}</a></li>
+                <li><a>{props.itens[4]}</a></li>
+            </ul>
+            </header>
+        </div>
+    );
+  }
--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,66 @@
+import React from "react";
 import { View } from "react-native";
+
+export default function Index() {
+  return (
+    
+    <View>
+      <FichaUsuario login = {'gabriel'} email = {'gabriel@gmail'} nome = {'gabrielName'} contatos={['(51) 995441888', '(55) 995441768']} ></FichaUsuario>
+      <Form_box   links={['home','Solicitar Guincho','Solicitações anteriores',"sair"]}
+                  aparecer
+      ></Form_box>
+      
+    </View>
+  );
+}
+function ListaContatos({contatos}){
+  return(
+    <>
+      <h2>contatos</h2>
+      <ul>
+        <li>{contatos[0]}</li>
+        <li>{contatos[1]}</li>
+      </ul>
+    
+    
+    </>
+
+  )
+}
+
+
+function ListaDatalhes({nome,email}){
+  return(
+    <>
+    <h2>datalhes</h2>
+    <ol>
+      <li>nome: {nome}</li>
+      <li>Email: {email}</li>
+    </ol>
+    </>
+  );
+
+}
+function FichaUsuario({login,nome,email,contatos}){
+  return(
+    <>
+      <h1>Ficha do usuario</h1>
+      <strong>Login: {login}</strong>
+      <ListaDatalhes nome = {nome} email = {email}></ListaDatalhes>
+      <ListaContatos contatos={contatos}></ListaContatos>
+      
+    </>
+  )
+}
 function Form_box (props){
   return(
   <footer>
       {props.aparecer? (
         <ul>
-            <li><a >{props.links[0]}</a></li>    
-            <li><a >{props.links[1]}</a></li>
-            <li ><a >{props.links[2]}</a></li>
-            <li ><a >{props.links[3]}</a></li>
+          <li><a >{props.links[0]}</a></li>    
+          <li><a >{props.links[1]}</a></li>
+          <li ><a >{props.links[2]}</a></li>
+          <li ><a >{props.links[3]}</a></li>
         </ul>) : 
             <h1>Você NÃO tem permissão para acessar esse sistema, SAIA!!!!!!</h1>
         }
@@ -16,13 +69,5 @@ function Form_box (props){
   </footer>
   );
 }
-export default function Index() {
-  return (
-    
-    <View>
-      <Form_box   links={['home','Solicitar Guincho','Solicitações anteriores',"sair"]}
-                  aparecer
-      ></Form_box>
-    </View>
-  );
-}
+
+
