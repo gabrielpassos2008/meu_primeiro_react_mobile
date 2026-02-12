ea44e63e1846ed5fddce84e19d17f3528659693b
gabrielpassos2008 <gabrielpassos2008@gmail>
2026-02-12 20:53:36 -0300

    adicionei duas novas funções, quando clica no botao aparece a lista

--- app/index.tsx
+++ app/index.tsx
@@ -5,10 +5,39 @@ export default function Index() {
   return (
     
     <ScrollView>
-        <CookieClicker></CookieClicker>
+        <CardLista nome= {'gabriel'}  lista = {['Coca-Cola','Suco de Laranja Natural','Água Mineral','Energético Red Bull']}></CardLista>
     </ScrollView>
   );
 }
+
+function CardLista({nome,lista}){
+  
+  const [aparecer, setTrue] = React.useState(false);
+
+  return(
+    <>
+     <h1>Seja bem vindo </h1>
+     <p>Nome: {nome} </p>
+     <button onClick={()=>setTrue(!aparecer)}>Datalhes</button>
+     {aparecer? <Lista lista={lista}></Lista>: null}
+    </>
+  )
+}
+
+function Lista({lista}){
+  return(
+
+  <>
+    <ol>
+      {lista.map(produtos => <li>{produtos}</li>)}
+    </ol>
+  </>
+
+  )
+}
+
+
+
 
 function CookieClicker(){
 const botoesExtras=<>
