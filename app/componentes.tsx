9c329a2ab8a30ea05eeb8597039d3ab6c4000419
gabrielpassos2008 <gabrielpassos2008@gmail>
2026-02-12 20:14:56 -0300

    rederização condicional

--- app/componentes.tsx
+++ app/componentes.tsx
@@ -13,3 +13,99 @@ export function Ohhh(props){
         </div>
     );
   }
+
+  function ListarProdutos({categoria,lista}){
+    return(
+      <>
+        <h2>categoria: {categoria}</h2>
+         <ol>
+            {
+                lista.map(produtos => <li>{produtos}</li>)
+            }
+         </ol>
+      </>
+    );
+  }  
+  
+  
+  
+  function Produtos({lista}){
+    return(
+      <>
+        <h1>Lista de Produtos</h1>
+        <ul>{
+          lista.map(produto => <li>{produto}</li>)
+        }
+        </ul>
+      </>
+  
+    )
+    
+  }
+  function BoxProdutos({data}){
+    return(
+      <>
+        <h1>suas lista de produtos:</h1>
+        <strong>data de hoje: {data}</strong>
+        <ListarProdutos categoria={"Bebidas"} produtos={['Coca-Cola','Suco de Laranja Natural','Água Mineral','Energético Red Bull']} ></ListarProdutos>
+        <ListarProdutos categoria={"Frutas"} produtos={['Maçã','Banana','Laranja','Melancia']} ></ListarProdutos>
+        <ListarProdutos categoria={"Produtos de limpeza"} produtos={['Detergente','Sabão em pó','Água sanitária (cloro)','Desinfetante']} ></ListarProdutos>
+      </>
+    );
+  }
+
+
+function ListaContatos({contatos}){
+  return(
+    <>
+      <h2>contatos</h2>
+      <ul>
+        <li>{contatos[0]}</li>
+        <li>{contatos[1]}</li>
+      </ul>
+    </>
+
+  )
+}
+
+function ListaDatalhes({nome,email}){
+  return(
+    <>
+    <h2>datalhes</h2>
+    <ol>
+      <li>nome: {nome}</li>
+      <li>Email: {email}</li>
+    </ol>
+    </>
+  );
+
+}
+function FichaUsuario({login,nome,email,contatos}){
+  return(
+    <>
+      <h1>Ficha do usuario</h1>
+      <strong>Login: {login}</strong>
+      <ListaDatalhes nome = {nome} email = {email}></ListaDatalhes>
+      <ListaContatos contatos={contatos}></ListaContatos>
+      
+    </>
+  )
+}
+
+function FormBox (props){
+  return(
+  <footer>
+      {props.aparecer? (
+        <ul>
+          <li><a >{props.links[0]}</a></li>    
+          <li><a >{props.links[1]}</a></li>
+          <li ><a >{props.links[2]}</a></li>
+          <li ><a >{props.links[3]}</a></li>
+        </ul>) : 
+            <h1>Você NÃO tem permissão para acessar esse sistema, SAIA!!!!!!</h1>
+        }
+      <hr />
+      <p >© 2025 — Desenvolvido por Gabriel</p>
+  </footer>
+  );
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,73 +1,43 @@
 import React from "react";
-import { View } from "react-native";
+import { ScrollView } from "react-native";
 
 export default function Index() {
   return (
     
-    <View>
-      <FichaUsuario login = {'gabriel'} email = {'gabriel@gmail'} nome = {'gabrielName'} contatos={['(51) 995441888', '(55) 995441768']} ></FichaUsuario>
-      <Form_box   links={['home','Solicitar Guincho','Solicitações anteriores',"sair"]}
-                  aparecer
-      ></Form_box>
-      
-    </View>
+    <ScrollView>
+        <CookieClicker></CookieClicker>
+    </ScrollView>
   );
 }
-function ListaContatos({contatos}){
+
+function CookieClicker(){
+const botoesExtras=<>
+  <button onClick= {diminuirCookie}> clique aqui para dimunir um cookie</button> 
+  <button onClick= {zerarCookie}> clique aqui para zerar um cookie</button> 
+</>
+const [quantidade,setQuantidade] = React.useState(0);
+
+function ganharCookie(){
+  setQuantidade(quantidade + 1);
+}
+function zerarCookie(){
+  setQuantidade(quantidade - quantidade)
+}
+
+function diminuirCookie(){
+  setQuantidade(quantidade - quantidade)
+}
+
   return(
     <>
-      <h2>contatos</h2>
-      <ul>
-        <li>{contatos[0]}</li>
-        <li>{contatos[1]}</li>
-      </ul>
-    
-    
+      <h1>Cookie Clicker</h1>
+      <p>quantidade de cookies: {quantidade}</p>
+      <button onClick= {ganharCookie}> clique aqui para ganhar um cookie</button> 
+      {quantidade? botoesExtras:null}
     </>
+
 
   )
-}
-
-
-function ListaDatalhes({nome,email}){
-  return(
-    <>
-    <h2>datalhes</h2>
-    <ol>
-      <li>nome: {nome}</li>
-      <li>Email: {email}</li>
-    </ol>
-    </>
-  );
-
-}
-function FichaUsuario({login,nome,email,contatos}){
-  return(
-    <>
-      <h1>Ficha do usuario</h1>
-      <strong>Login: {login}</strong>
-      <ListaDatalhes nome = {nome} email = {email}></ListaDatalhes>
-      <ListaContatos contatos={contatos}></ListaContatos>
-      
-    </>
-  )
-}
-function Form_box (props){
-  return(
-  <footer>
-      {props.aparecer? (
-        <ul>
-          <li><a >{props.links[0]}</a></li>    
-          <li><a >{props.links[1]}</a></li>
-          <li ><a >{props.links[2]}</a></li>
-          <li ><a >{props.links[3]}</a></li>
-        </ul>) : 
-            <h1>Você NÃO tem permissão para acessar esse sistema, SAIA!!!!!!</h1>
-        }
-      <hr />
-      <p >© 2025 — Desenvolvido por Gabriel</p>
-  </footer>
-  );
 }
 
 
